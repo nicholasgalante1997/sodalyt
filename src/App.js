3a224848924e0c1d34319597a52555a0e8c2fdd5
nicholasgalante1997 <nicholasgalante1997@gmail.com>
2020-10-12 17:37:53 -0400

    test-fetch

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import { Switch, Route } from 'react-router-dom'
 
+const testURL = 'https://61msatt1jf.execute-api.us-east-2.amazonaws.com/Beta'
+
+async function fetchData(){
+  fetch(testURL)
+  .then(r => r.json()
+  .then(data => {
+    console.log(data)
+  }))
+}
+
 function App() {
+  fetchData()
   return (
     <div className="App">
         <h3>Sodalyt</h3>
