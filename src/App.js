ad42f59c44af0f590cf29be08899fd80b5772c61
nicholasgalante1997 <nicholasgalante1997@gmail.com>
2020-10-12 16:35:34 -0400

    test-react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
         <h3>Sodalyt</h3>
+        <Switch>
+          <Route exact path='/test' render={() => (<div>this is a test page</div>)} />
+        </Switch>
     </div>
   );
 }
